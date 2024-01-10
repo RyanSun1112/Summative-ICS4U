0ebee4d73298777b6972764b7c9f2f5b253cb63f
Dinuri <dgard1@ocdsb.ca>
2024-01-10 11:17:34 -0500

    yay :)

---
+++ src/homePage.java
@@ -0,0 +1,65 @@
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+public class homePage {
+    //INSTANCE VARIABLES
+    private JFrame frame = new JFrame("Chemistry Galore!");
+    private Color white = new Color(255,255,255);
+
+    //FONT METHODS
+    private Font theNormalFont(int size)  {
+
+        GraphicsEnvironment graphics = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        URL url = classloader.getResource("CaviarDreams/CaviarDreams.ttf");
+        File fontFile = null;
+        try {
+            fontFile = new File(url.toURI());
+        } catch(Exception e) {
+            System.out.println("Something");
+        }
+
+        Font caviarDreams = null;
+
+        try {
+            caviarDreams = Font.createFont(Font.TRUETYPE_FONT,fontFile).deriveFont(Font.BOLD,size);
+            graphics.registerFont(Font.createFont(Font.TRUETYPE_FONT, fontFile));
+            return caviarDreams;
+        } catch (IOException | FontFormatException e) {
+            System.out.println("ERROR! Code in 'e.printStackTrace()' to print stack trace: ");
+        }
+
+        return caviarDreams;
+    }
+
+    private void formatButton(JButton button) {
+        button.setFont(theNormalFont(35));
+        button.setFocusable(false);
+        button.setBorder(BorderFactory.createEmptyBorder());
+        button.setOpaque(false);
+        button.setContentAreaFilled(false);
+        button.setBorderPainted(false);
+        button.setForeground(white);
+
+    }
+
+    //HOME PAGE METHOD
+    public void start() {
+        JLayeredPane pane = new JLayeredPane();
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        URL url = classloader.getResource("ChemistryGalore_homePage.png");
+        Icon backgroundIcon = new ImageIcon(url);
+        JLabel background = new JLabel(backgroundIcon);
+
+        background.setBounds(0,0,1152,648);
+        pane.add(background,JLayeredPane.DEFAULT_LAYER);
+
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.add(pane);
+        frame.setSize(1152,678);
+        frame.setVisible(true);
+    }
+}
