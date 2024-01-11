b6f3af5a46bc801196e6f090ece5ce469cdce1b3
Dinuri <dgard1@ocdsb.ca>
2024-01-11 09:31:55 -0500

    yay :)

--- src/readySetGo!.java
+++ src/readySetGo!.java
@@ -4,8 +4,8 @@ import java.io.File;
 
 class homePage {
     //INSTANCE VARIABLES
-    private JFrame frame = new JFrame("Chemistry Galore!");
-    private Color white = new Color(255,255,255);
+    private final JFrame frame = new JFrame("Chemistry Galore!");
+
 
     //HOME PAGE METHOD
     public void start() {
@@ -21,9 +21,37 @@ class homePage {
 }
 
 class choices {
+    //INSTANCE VARIABLES
+    private final JFrame frame = new JFrame("Chemistry Galore!~ Choose your adventure!");
 
+
+    //CHOICES PAGE METHOD
+    public void choicesPage() {
+        JLayeredPane pane = new JLayeredPane();
+        JLabel background = Design.setBackgroundImage("ChemistryGalore!/ChemistryGalore_optionsPage.png");
+        pane.add(background,JLayeredPane.DEFAULT_LAYER);
+
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.add(pane);
+        frame.setSize(1152,678);
+        frame.setVisible(true);
+    }
 }
 
 class howToPlay {
+    //INSTANCE VARIABLES
+    private final JFrame frame = new JFrame("Chemistry Galore! ~ How to wield this scepter!");
 
+
+    //INSTRUCTIONS PAGE METHOD
+    public void howToPlayPage() {
+        JLayeredPane pane = new JLayeredPane();
+        JLabel background = Design.setBackgroundImage("ChemistryGalore!/ChemistryGalore_howToPlay.png");
+        pane.add(background,JLayeredPane.DEFAULT_LAYER);
+
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.add(pane);
+        frame.setSize(1152,678);
+        frame.setVisible(true);
+    }
 }
