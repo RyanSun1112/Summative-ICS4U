ddd58ff4118042c55f8c8d33ef63be41088bf0f0
Dinuri <dgard1@ocdsb.ca>
2024-01-12 18:58:51 -0500

    yay :)

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        //HomePage home = new HomePage();
-        //home.start();
-        BalancingChemicalEquations_V1 bce = new BalancingChemicalEquations_V1();
-        bce.page();
+        HomePage home = new HomePage();
+        home.start();
 
     }
 }
