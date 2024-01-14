764212ce8a3f98abddb9b4c3ae5bd39d692b7475
Dinuri <dgard1@ocdsb.ca>
2024-01-13 20:59:40 -0500

    yay :)

--- src/BalancingChemicalEquations.java
+++ src/BalancingChemicalEquations.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
 class BalancingChemicalEquations_V1 {
@@ -13,6 +15,47 @@ class BalancingChemicalEquations_V1 {
         JLayeredPane pane = new JLayeredPane();
 
         Design.QuickMenu1(pane,frame);
+
+        JButton back = new JButton("Back?");
+        back.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.setVisible(false);
+                HomePage home = new HomePage();
+                home.start();
+            }
+        });
+        Design.formatButton(back,30);
+        back.setBounds(620,465,155,120);
+        pane.add(back);
+
+        JButton next = new JButton("Next!");
+        next.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.setVisible(false);
+                BalancingChemicalEquations_Answer answer = new BalancingChemicalEquations_Answer();
+                answer.bceAnswerPage();
+            }
+        });
+        Design.formatButton(next,30);
+        next.setBounds(800,465,155,120);
+        pane.add(next);
+
+        JButton noFormula = new JButton("Don't know what the formula is?" +
+                "<BR>That's fine, click HERE to enter just<BR>the reactants and type of reaction," +
+                "<BR>then we'll do the rest!");
+        noFormula.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.setVisible(false);
+                BalancingChemicalEquations_V2 answer = new BalancingChemicalEquations_V2();
+                answer.bcePage();
+            }
+        });
+        Design.formatButton(noFormula,20);
+        noFormula.setBounds(60,345,455,320);
+        pane.add(noFormula);
 
         JLabel background = Design.setBackgroundImage("ChemistryGalore!/ChemistryGalore_BCE1.png");
         pane.add(background,JLayeredPane.DEFAULT_LAYER);
@@ -39,6 +82,48 @@ class BalancingChemicalEquations_V2 {
 
         Design.QuickMenu1(pane,frame);
 
+
+        JButton back = new JButton("Back?");
+        back.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.setVisible(false);
+                HomePage home = new HomePage();
+                home.start();
+            }
+        });
+        Design.formatButton(back,30);
+        back.setBounds(620,465,155,120);
+        pane.add(back);
+
+        JButton next = new JButton("Next!");
+        next.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.setVisible(false);
+                BalancingChemicalEquations_Answer answer = new BalancingChemicalEquations_Answer();
+                answer.bceAnswerPage();
+            }
+        });
+        Design.formatButton(next,30);
+        next.setBounds(800,465,155,120);
+        pane.add(next);
+
+        JButton withFormula = new JButton("Want to enter the unbalanced" +
+                "<BR>formula instead? Click HERE to enter<BR>the entire chemical equation and," +
+                "<BR>type of reaction!");
+        withFormula.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.setVisible(false);
+                BalancingChemicalEquations_V1 answer = new BalancingChemicalEquations_V1();
+                answer.bcePage();
+            }
+        });
+        Design.formatButton(withFormula,20);
+        withFormula.setBounds(60,345,455,320);
+        pane.add(withFormula);
+
         JLabel background = Design.setBackgroundImage("ChemistryGalore!/ChemistryGalore_BCE2.png");
         pane.add(background,JLayeredPane.DEFAULT_LAYER);
 
@@ -58,11 +143,37 @@ class BalancingChemicalEquations_Answer {
 
 
     //BALANCING CHEMICAL EQUATIONS ANSWER METHOD
-    public void bcePage() {
+    public void bceAnswerPage() {
 
         JLayeredPane pane = new JLayeredPane();
 
         Design.QuickMenu1(pane,frame);
+
+        JButton back = new JButton("Back?");
+        back.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.setVisible(false);
+                BalancingChemicalEquations_V1 bce = new BalancingChemicalEquations_V1();
+                bce.bcePage();
+            }
+        });
+        Design.formatButton(back,30);
+        back.setBounds(510,425,155,120);
+        pane.add(back);
+
+        JButton understand = new JButton("Don't quite understand why? We've got your back! Click HERE for an explanation!");
+        understand.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.setVisible(false);
+                BalancingChemicalEquations_Explained see = new BalancingChemicalEquations_Explained();
+                see.bcePage();
+            }
+        });
+        Design.formatButton(understand,20);
+        understand.setBounds(140,510,900,120);
+        pane.add(understand);
 
         JLabel background = Design.setBackgroundImage("ChemistryGalore!/ChemistryGalore_BCEdrumroll.png");
         pane.add(background,JLayeredPane.DEFAULT_LAYER);
@@ -88,6 +199,19 @@ class BalancingChemicalEquations_Explained {
         JLayeredPane pane = new JLayeredPane();
 
         Design.QuickMenu1(pane,frame);
+
+        JButton back = new JButton("Back?");
+        back.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.setVisible(false);
+                BalancingChemicalEquations_V1 bce = new BalancingChemicalEquations_V1();
+                bce.bcePage();
+            }
+        });
+        Design.formatButton(back,30);
+        back.setBounds(535,190,155,120);
+        pane.add(back);
 
         JLabel background = Design.setBackgroundImage("ChemistryGalore!/ChemistryGalore_BCEexplained.png");
         pane.add(background,JLayeredPane.DEFAULT_LAYER);
