7b122f684d56ee4370a6c13d99063d7dfd60fc1d
Dinuri <dgard1@ocdsb.ca>
2024-01-20 18:22:13 -0500

    yay :)

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,6 @@ public class Main {
     public static void main(String[] args) {
         HomePage home = new HomePage();
         home.start();
-//        HowToPlay play = new HowToPlay();
-//        play.howToPlayPage();
 
     }
 }
